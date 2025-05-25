3d2f250c752d3b9166be0467db9f195a08115972
Owen <ojr4316@rit.edu>
2025-05-25 00:02:42 -0400

    refactor

--- hardware/screen.py
+++ hardware/screen.py
@@ -1,76 +1,90 @@
 """ Screen Manager for 1.3" Adafruit TFT Bonnet """
-import time
-import random
-from colorsys import hsv_to_rgb
-import board
-from digitalio import DigitalInOut, Direction
-from PIL import Image, ImageDraw, ImageFont
-from adafruit_rgb_display import st7789
+import os
+if os.name != 'nt' and os.uname().nodename == "rpi":
+    import time
+    import random
+    from colorsys import hsv_to_rgb
+    import board
+    from digitalio import DigitalInOut, Direction
+    from PIL import Image, ImageDraw, ImageFont
+    from adafruit_rgb_display import st7789
 
-class Screen:
-    cs_pin = DigitalInOut(board.CE0)
-    dc_pin = DigitalInOut(board.D25)
-    reset_pin = DigitalInOut(board.D24)
-    BAUDRATE = 24000000
+    class Screen:
+        cs_pin = DigitalInOut(board.CE0)
+        dc_pin = DigitalInOut(board.D25)
+        reset_pin = DigitalInOut(board.D24)
+        BAUDRATE = 24000000
 
-    spi = board.SPI()
-    disp = st7789.ST7789(
-        spi,
-        height=240,
-        y_offset=80,
-        rotation=180,
-        cs=cs_pin,
-        dc=dc_pin,
-        rst=reset_pin,
-        baudrate=BAUDRATE,
-    )
+        spi = board.SPI()
+        disp = st7789.ST7789(
+            spi,
+            height=240,
+            y_offset=80,
+            rotation=180,
+            cs=cs_pin,
+            dc=dc_pin,
+            rst=reset_pin,
+            baudrate=BAUDRATE,
+        )
 
-    # Input pins:
-    button_A = DigitalInOut(board.D5)
-    button_A.direction = Direction.INPUT
+        # Input pins:
+        button_A = DigitalInOut(board.D5)
+        button_A.direction = Direction.INPUT
 
-    button_B = DigitalInOut(board.D6)
-    button_B.direction = Direction.INPUT
+        button_B = DigitalInOut(board.D6)
+        button_B.direction = Direction.INPUT
 
-    button_L = DigitalInOut(board.D27)
-    button_L.direction = Direction.INPUT
+        button_L = DigitalInOut(board.D27)
+        button_L.direction = Direction.INPUT
 
-    button_R = DigitalInOut(board.D23)
-    button_R.direction = Direction.INPUT
+        button_R = DigitalInOut(board.D23)
+        button_R.direction = Direction.INPUT
 
-    button_U = DigitalInOut(board.D17)
-    button_U.direction = Direction.INPUT
+        button_U = DigitalInOut(board.D17)
+        button_U.direction = Direction.INPUT
 
-    button_D = DigitalInOut(board.D22)
-    button_D.direction = Direction.INPUT
+        button_D = DigitalInOut(board.D22)
+        button_D.direction = Direction.INPUT
 
-    button_C = DigitalInOut(board.D4)
-    button_C.direction = Direction.INPUT
+        button_C = DigitalInOut(board.D4)
+        button_C.direction = Direction.INPUT
 
-    backlight = DigitalInOut(board.D26)
-    backlight.switch_to_output()
-    backlight.value = True
+        backlight = DigitalInOut(board.D26)
+        backlight.switch_to_output()
+        backlight.value = True
 
-    width = disp.width
-    height = disp.height
-    image = Image.new("RGB", (width, height))
+        width = disp.width
+        height = disp.height
+        image = Image.new("RGB", (width, height))
 
-    draw = ImageDraw.Draw(image)
+        draw = ImageDraw.Draw(image)
 
-    fnt = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
+        fnt = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 18)
 
-    frame = 0
+        frame = 0
 
-    def draw_screen(self, img):
-        self.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=(0, 0, 0))
+        def draw_screen(self, img):
+            self.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=(0, 0, 0))
 
-        if img is not None:
-            resize = img.resize((240, 240))
-            self.image.paste(resize, (0, 0))
-            #self.frame+=1
-        #self.draw.text((0, 0), "Frame: " + str(self.frame), font=self.fnt, fill=(255, 255, 255))
+            if img is not None:
+                resize = img.resize((240, 240))
+                self.image.paste(resize, (0, 0))
+                #self.frame+=1
+            #self.draw.text((0, 0), "Frame: " + str(self.frame), font=self.fnt, fill=(255, 255, 255))
 
-        self.disp.image(self.image)        
+            self.disp.image(self.image)        
 
-    def handle_input(self, input):
-        input.update(not self.button_A.value, not self.button_B.value, not self.button_L.value, not self.button_R.value, not self.button_U.value, not self.button_D.value, not self.button_C.value)
+        def handle_input(self, input):
+            input.update(not self.button_A.value, not self.button_B.value, not self.button_L.value, not self.button_R.value, not self.button_U.value, not self.button_D.value, not self.button_C.value)
+
+
+
+
+else: # Fake Screen
+
+    class Screen:
+
+        def __init__(self):
+            self.width = 240
+            self.height = 240
+            #self.image = Image.new("RGB", (self.width, self.height))

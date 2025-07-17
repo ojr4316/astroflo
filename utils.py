549a7d17812b25fa9e5f5a033745f9b8bdfabe49
Owen <ojr4316@rit.edu>
2025-07-16 22:41:50 -0400

    temp formulas

--- utils.py
+++ utils.py
@@ -21,3 +21,44 @@ def radec_to_vector(ra_deg:float, dec_deg:float):
     z = np.sin(dec)
 
     return np.array([x, y, z])
+
+def solve_rotation(camera, telescope):
+    assert camera.shape == telescope.shape
+
+    H = camera @ telescope.T
+    U, _, Vt = np.linalg.svd(H)
+    R = Vt.T @ U.T
+
+    if np.linalg.det(R) < 0:
+        Vt[-1, :] *= -1
+        R = Vt.T @ U.T
+    return R
+
+def apply_rotation(R, ra, dec):
+    vec = radec_to_vector(ra, dec)
+    telescope = R @ vec
+
+    x, y, z = telescope
+    dec = np.degrees(np.arcsin(z))
+    ra = np.degrees(np.arctan2(y, x)) % 360
+
+    return ra, dec
+
+def haversine_dist(current_ra, current_dec, target_ra, target_dec, roll=0):
+    current_ra_rad = np.radians(current_ra)
+    current_dec_rad = np.radians(current_dec)
+    target_ra_rad = np.radians(target_ra)
+    target_dec_rad = np.radians(target_dec)
+
+    delta_ra = target_ra_rad - current_ra_rad
+    delta_dec = target_dec_rad - current_dec_rad
+
+    a = (np.sin(delta_dec / 2) ** 2 +
+         np.cos(current_dec_rad) * np.cos(target_dec_rad) * (np.sin(delta_ra / 2) ** 2))
+    c = 2 * np.arcsin(np.sqrt(a))
+
+    roll_adjustment = np.radians(roll)
+    c += roll_adjustment
+
+    return c
+   

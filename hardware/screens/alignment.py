8c7c65c76b9a382736f885e7fe8a684c77f44e1a
Owen <ojr4316@rit.edu>
2025-07-23 14:41:11 -0400

    temp alignment

--- hardware/screens/alignment.py
+++ hardware/screens/alignment.py
@@ -3,7 +3,12 @@ from PIL import Image, ImageDraw
 from hardware.state import ScreenState
 
 class AlignmentScreen(Screen):
-        
+    
+    def __init__(self, ui):
+        super().__init__(ui)
+        self.current_target = ui.pipeline.solver.target_pixel
+
+
     def setup_input(self):
         self.screen_input.controls['A']["press"] = self.select
         self.screen_input.controls['B']["press"] = self.alt_select
@@ -14,55 +19,51 @@ class AlignmentScreen(Screen):
         self.screen_input.controls['R']["press"] = self.left
 
     def left(self):
-        current_target = self.pipeline.solver.target_pixel
+        current_target = self.current_target
         if current_target is not None:
             # Move the target pixel left
-            current_target = (current_target[0] - 1, current_target[1])
+            self.current_target = (current_target[0] - 1, current_target[1])
             print(f"Target pixel moved left to {current_target}")
-            self.pipeline.solver.target_pixel = current_target
 
     def right(self):
-        current_target = self.pipeline.solver.target_pixel
+        current_target = self.current_target
         if current_target is not None:
             # Move the target pixel right
-            current_target = (current_target[0] + 1, current_target[1])
+            self.current_target = (current_target[0] + 1, current_target[1])
             print(f"Target pixel moved right to {current_target}")
-            self.pipeline.solver.target_pixel = current_target
+
     def up(self):
-        current_target = self.pipeline.solver.target_pixel
+        current_target = self.current_target
         if current_target is not None:
             # Move the target pixel up
-            current_target = (current_target[0], current_target[1] - 1)
+            self.current_target = (current_target[0], current_target[1] - 1)
             print(f"Target pixel moved up to {current_target}")
-            self.pipeline.solver.target_pixel = current_target
+            
     def down(self):
-        current_target = self.pipeline.solver.target_pixel
+        current_target = self.current_target
         if current_target is not None:
             # Move the target pixel down
-            current_target = (current_target[0], current_target[1] + 1)
+            self.current_target = (current_target[0], current_target[1] + 1)
             print(f"Target pixel moved down to {current_target}")
-            self.pipeline.solver.target_pixel = current_target
 
     def alt_select(self):
         self.ui.change_screen(ScreenState.MAIN_MENU)
 
     def select(self):
+        self.pipeline.solver.target_pixel = self.current_target
+        print(f"Target pixel set to {self.current_target}")
         self.ui.change_screen(ScreenState.NAVIGATE)
    
     def render(self):
         pipeline = self.pipeline
-        current_target = pipeline.solver.target_pixel
-        
+        current_target = self.current_target
+
         if pipeline.latest_image is None:
             return self.renderer.render_many_text(["Waiting for first image..."])
         
         if current_target is None:
             #current_target = (512/2, 512/2) # default center
-            current_target = pipeline.find_target_pixel()
-            pipeline.solver.target_pixel = current_target
-
-        
-
+            self.current_target = pipeline.find_target_pixel()
 
         # draw the target pixel on the latest image
         latest_image = Image.fromarray(pipeline.latest_image)
